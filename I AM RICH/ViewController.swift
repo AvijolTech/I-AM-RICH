a68a91925ff8fe3862bfabd09687f77b221656a8
raghu k <raghutejak@raghus-macbook-air.local>
2018-11-23 17:38:38 +0530

    Initial Commit

---
+++ I AM RICH/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I AM RICH
+//
+//  Created by raghu teja k on 06/11/18.
+//  Copyright © 2018 raghu teja k. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
